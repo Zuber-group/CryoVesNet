12e9f1040cd5d66f055ae7c9d40ef18989ffd4a0
ameen-khosrowzadeh <ameen-khosrowzadeh@users.noreply.github.com>
2020-10-27 00:24:54 +0100

    directory fixing

--- complete_vesicle_segmentation.py
+++ complete_vesicle_segmentation.py
@@ -2,17 +2,19 @@ import os
 import unetmic.segment as segment
 from pathlib import Path
 import tensorflow as tf
+import shutil
 
 def vesicle_segmentation(path_to_folder):
     os.chdir('../..')
     dataset_dir = os.getcwd()
     unet_weigth_path = dataset_dir + '/weights/weights.h5'
     os.chdir('./data/'+path_to_folder)
-    print("XXXXXXXX",os.getcwd())
-    if os.path.exists('./deep'):
-        print("XXXXXXXX", os.getcwd())
-        os.rmdir('deep')
-    os.mkdir('deep')
+
+    if os.path.exists('./' + '/deep'):
+        shutil.rmtree('deep', ignore_errors=True)
+        os.mkdir('deep')
+    else:
+        os.mkdir('deep')
     cwd = Path('.')
     path_generator = cwd.glob('*.rec')
     file_name = ([str(x) for x in path_generator])[0]
@@ -28,4 +30,4 @@ def vesicle_segmentation(path_to_folder):
 
     # set network size
     network_size = 64
-    segment.full_segmentation(network_size, unet_weigth_path, path_to_file, folder_to_save, rescale=0.5, gauss=True)
+    # segment.full_segmentation(network_size, unet_weigth_path, path_to_file, folder_to_save, rescale=0.5, gauss=True)
