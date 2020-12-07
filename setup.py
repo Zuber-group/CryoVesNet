609069b4028c0b5ad968043de03929cdec182476
aseedb <31647043+aseedb@users.noreply.github.com>
2020-12-07 16:03:05 +0100

    added setup.py and requirements.txt for installing with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='prepyto',
+    version='',
+    packages=['weights', 'pyto_scripts', 'pyto_scripts.3d'],
+    url='',
+    license='',
+    author='Amin Khosrozadeh',
+    author_email='',
+    description='prepyto - branch pathlib',
+)
