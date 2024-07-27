d013156133d90a0603255921f1c2f9f25947fc98
amineuron <amin.khosrowzadeh@icloud.com>
2024-07-27 23:36:55 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cryovesnet',
-    version='0.2.0',
-    url='',
+    name='CryoVesNet',
+    version='0.5.0',
+    url='https://github.com/Zuber-group/CryoVesNet',
     packages=find_packages(),
-    license='',
-    author='Amin Khosrozadeh',
-    author_email='',
-    description='cryovesnet - branch pathlib',
+    license='MIT',  # Please confirm or change the license as appropriate
+    author='Amin Khosrozadeh, Guillaume Witz, Benoît Zuber',
+    author_email='amin.khosrozadeh@unibe.ch',
+    description='CryoVesNet: A Dedicated Framework for Synaptic Vesicle Segmentation in Cryo Electron Tomograms',
     package_data={'': ['weights/weights.h5']},
     include_package_data=True,
-    install_requires=['numpy', 'scikit-image', 'scipy', 'jupyter','jupyterlab',
-                      'pandas', 'h5py', 'tifffile','mrcfile','tqdm', 'napari[pyqt5]',
-                      'napari-mrcfile-reader','keras==2.9.0','tensorflow<2.10'],
-
+    install_requires=[
+        'numpy',
+        'scikit-image',
+        'scipy',
+        'jupyter',
+        'jupyterlab',
+        'pandas',
+        'h5py',
+        'tifffile',
+        'mrcfile',
+        'tqdm',
+        'napari[pyqt5]',
+        'napari-mrcfile-reader',
+        'keras==2.9.0',
+        'tensorflow<2.10'
+    ],
+    python_requires='>=3.10',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
 )
